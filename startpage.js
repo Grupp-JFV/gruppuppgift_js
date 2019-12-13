6ec647b9921310fb0821fc11134cf62df66238e5
josefinljung <58325534+josefinljung@users.noreply.github.com>
2019-12-13 11:38:21 +0100

    startsida toggle

---
+++ startpage.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+$("#readmoreindex_button").on("click", function(){
+    $(".readmoreindex").slideToggle(300);
+
+
+
+
+}); //stänger klick-funktionen
+
+
+
+
+}); //stänger document onload
